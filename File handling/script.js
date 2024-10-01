b1fea43dd55f37a575ce4dbf1d2baa32f84ccabd
Maaz Ali <Maaztarkalanii@gmail.com>
2024-10-02 01:19:47 +0500

    Your commit message here

---
+++ File handling/script.js
@@ -0,0 +1,8 @@
+const fs = require("node:fs");
+fs.writeFile("File1.txt", "Hello! what's going on......", function (err) {
+  if (err) {
+    console.log(`can't create file${err}`);
+  } else {
+    console.log("Successfully created the file");
+  }
+});
